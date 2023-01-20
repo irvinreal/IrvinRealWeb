0e70d57146bac582ad978a7e7279d6a046821461
Irvin Gonzalez Real <irvin.glez.real@hotmail.com>
2023-01-20 12:21:23 -0600

    Se actualiza el Proyecto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// 
+const logotipo_nav = document.querySelector('.logotipo-nav')
+const ul = document.querySelector('.ul')
+
+logotipo_nav.addEventListener('click', () => {
+    if (ul.classList.contains('open')) {
+        ul.classList.remove('open')
+        ul.classList.remove('on')
+        logotipo_nav.classList.remove('on')
+
+    } else  {
+    ul.classList.add('open')
+    ul.classList.add('on')
+    logotipo_nav.classList.add('on')
+    }
+})
